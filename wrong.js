3481df96e957101da3292bb550d5204dbe1e5ec3
Devin Rodriguez <devin@scaryuncledevin.com>
2017-04-14 12:45:32 -0700

    Removed dependency on gulp-color, bumped version number

--- wrong.js
+++ wrong.js
@@ -1,14 +1,26 @@
-var color = require('gulp-color');
 var lines = require('./lines.json');
 
-module.exports = function() {
+const colors = {
+    BLACK:'\x1b[30m',
+    RED:'\x1b[31m',
+    YELLOW:'\x1b[33m',
+    BLUE:'\x1b[34m',
+    WHITE:'\x1b[37m',
+    CLEAR:'\x1b[0m'
+}
+
+function wrong () {
     lines.forEach(function(line){
         var output = "";
 
         line.forEach(function(block){
-            output += color('#'.repeat(block.l), block.c)
+            output +=  colors[block.c.toUpperCase()] + '#'.repeat(block.l) + colors.CLEAR
         })
 
         console.log(output);
     })
 }
+
+module.exports = wrong;
+
+wrong();
